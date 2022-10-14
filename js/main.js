6c9832aa76334ba3f6d2006d68c5368dbaea5f12
Nikita Fomkin <nikita.fomkin@list.ru>
2022-10-15 02:12:50 +0300

    feat: add content

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $(".header__burger").click(function (event) {
-    $(".header__burger, .header__mobile-menu").toggleClass('active');
-  })
+    $("#burger-menu").click(function (event) {
+        $(".opened-burger-menu").toggleClass('active');
+        $("#burger-menu").toggleClass('hidden');
+    })
 
-  $(".menu__close").click(function (event) {
-    $(".header__burger, .header__mobile-menu").toggleClass('active');
-  })
+    $(".menu__close").click(function (event) {
+        $(".opened-burger-menu").toggleClass('active');
+        $("#burger-menu").toggleClass('hidden');
+    })
+
+    $(window).on("scroll", function () {
+        if (window.matchMedia("(min-width: 1025px)").matches) {
+            $(".header-alternative").toggleClass('active', $(window).scrollTop() > $(".header").height());
+            // $(".header-alternative").toggleClass('active', $(window).scrollTop() < $(".header").height());
+        }
+    })
+
+
+    $(".hero__bg").css("min-height", $(window).height() - $(".header").height());
 });
